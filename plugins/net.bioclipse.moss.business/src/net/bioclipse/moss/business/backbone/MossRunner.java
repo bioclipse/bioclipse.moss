c0467b42d34f4e41c2df9c9806bc59e907eaacb7
annzi <annzi.andersson@gmail.com>
2010-06-04 14:32:36 +0200

    Added copyright lines to files

--- plugins/net.bioclipse.moss.business/src/net/bioclipse/moss/business/backbone/MossRunner.java
+++ plugins/net.bioclipse.moss.business/src/net/bioclipse/moss/business/backbone/MossRunner.java
@@ -1,11 +1,12 @@
-/*******************************************************************************
- *Copyright (c) 2008 The Bioclipse Team and others.
- *All rights reserved. This program and the accompanying materials
- *are made available under the terms of the Eclipse Public License v1.0
- *which accompanies this distribution, and is available at
- *http://www.eclipse.org/legal/epl-v10.html
+/* Copyright (c) 2010  Annsofie Andersson <annzi.andersson@gmail.com>
  *
- *******************************************************************************/
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: http://www.bioclipse.net/
+ */
 package net.bioclipse.moss.business.backbone;
 
 /**
